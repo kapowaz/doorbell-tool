3608bdf61ec2d27a2f4ed9f1a067a984ade758b1
Ben Darlow <github@kapowaz.net>
2025-10-17 00:38:59 +0100

    Bin the rubbish ancient eslint-config-standard

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,4 @@
 import { defineConfig } from 'eslint/config';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
 import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default defineConfig([
-  {
-    extends: compat.extends('standard'),
-  },
-  tseslint.configs.recommended,
-]);
+export default defineConfig([tseslint.configs.recommended]);
