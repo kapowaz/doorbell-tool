99c3689b0f0b0e11feb9005bbf35fd313a7f44cc
Ben Darlow <github@kapowaz.net>
2025-10-17 20:08:04 +0100

    Improve try…catch nesting for updateChime

--- src/tools/updateChime.ts
+++ src/tools/updateChime.ts
@@ -5,7 +5,7 @@ import util from 'util';
 import yargs from 'yargs';
 import prettier from 'prettier';
 import type { UBNTSoundConfig } from './types';
-import { logSuccess, logError } from './utils';
+import { logSuccess, logError } from './utils.ts';
 
 const SOUNDS_CONFIG_DEFAULTS = './src/tools/defaults/ubnt_sounds_leds.json';
 const OUTPUT_PATH = './build';
@@ -39,65 +39,65 @@ const updateChime = async () => {
   const soundFileSourcePath = `src/chimes/${soundFileName}`;
   const soundFileDestinationPath = `../sounds/${soundFileName}`;
 
-  if (fs.existsSync(soundFileSourcePath)) {
-    const soundsConfigOutput = JSON.stringify({
-      ...soundsConfigDefaults,
-      customSounds: [
-        {
-          ...soundsConfigDefaults.customSounds[0],
-          file: soundFileDestinationPath,
-          repeatTimes,
-          volume,
-        },
-      ],
-    });
+  const soundsConfigOutput = JSON.stringify({
+    ...soundsConfigDefaults,
+    customSounds: [
+      {
+        ...soundsConfigDefaults.customSounds[0],
+        file: soundFileDestinationPath,
+        repeatTimes,
+        volume,
+      },
+    ],
+  });
 
-    const outputFileName = `${OUTPUT_PATH}/ubnt_sounds_leds.conf`;
-    const formattedOutput = await prettier.format(soundsConfigOutput, { parser: 'json' });
+  const outputFileName = `${OUTPUT_PATH}/ubnt_sounds_leds.conf`;
+  const formattedOutput = await prettier.format(soundsConfigOutput, { parser: 'json' });
+
+  /**
+   * Generate a new sound config file
+   */
+
+  try {
+    if (!fs.existsSync(soundFileSourcePath)) {
+      throw new Error(
+        `The specified sound file ${colors.bold(soundFileSourcePath)} does not exist`,
+      );
+    }
+
+    fs.writeFileSync(outputFileName, formattedOutput, 'utf8');
+    logSuccess(`Wrote sounds config file to: ${colors.bold(outputFileName)}`);
 
     /**
-     * Generate a new sound config file
+     * Copy sounds config file to device
      */
+    logSuccess(
+      `Copying sounds config file to ${colors.bold(SOUNDS_CONFIG_FILE_DESTINATION_PATH)}…`,
+    );
 
-    try {
-      fs.writeFileSync(outputFileName, formattedOutput, 'utf8');
-      logSuccess(`Wrote sounds config file to: ${colors.bold(outputFileName)}`);
+    exec(
+      `sshpass -p $G4_DOORBELL_SSH_PASSWORD scp -O ${outputFileName} ubnt@$G4_DOORBELL_HOSTNAME:${SOUNDS_CONFIG_FILE_DESTINATION_PATH}`,
+    );
 
-      /**
-       * Copy sounds config file to device
-       */
-      logSuccess(
-        `Copying sounds config file to ${colors.bold(SOUNDS_CONFIG_FILE_DESTINATION_PATH)}…`,
-      );
+    /**
+     * Copy custom chime sound file to doorbell device
+     */
+    logSuccess(`Copying custom chime sound file ${colors.bold(soundFileName)} to doorbell device…`);
 
-      exec(
-        `sshpass -p $G4_DOORBELL_SSH_PASSWORD scp -O ${outputFileName} ubnt@$G4_DOORBELL_HOSTNAME:${SOUNDS_CONFIG_FILE_DESTINATION_PATH}`,
-      );
+    exec(
+      `sshpass -p $G4_DOORBELL_SSH_PASSWORD scp -O ${soundFileSourcePath} ubnt@$G4_DOORBELL_HOSTNAME:${SOUND_FILE_DESTINATION_PATH}`,
+    );
 
-      /**
-       * Copy custom chime sound file to doorbell device
-       */
-      logSuccess(
-        `Copying custom chime sound file ${colors.bold(soundFileName)} to doorbell device…`,
-      );
+    /**
+     * Restart the /bin/ubnt_sounds_leds process
+     */
+    logSuccess(`Restarting ${colors.bold(PROCESS_PATH)} process…`);
 
-      exec(
-        `sshpass -p $G4_DOORBELL_SSH_PASSWORD scp -O ${soundFileSourcePath} ubnt@$G4_DOORBELL_HOSTNAME:${SOUND_FILE_DESTINATION_PATH}`,
-      );
-
-      /**
-       * Restart the /bin/ubnt_sounds_leds process
-       */
-      logSuccess(`Restarting ${colors.bold(PROCESS_PATH)} process…`);
-
-      exec(
-        `sshpass -p $G4_DOORBELL_SSH_PASSWORD ssh ubnt@$G4_DOORBELL_HOSTNAME -f 'killall ${PROCESS_PATH}'`,
-      );
-    } catch (error) {
-      logError(`${error}`);
-    }
-  } else {
-    logError(`The specified sound file ${colors.bold(soundFileSourcePath)} does not exist`);
+    exec(
+      `sshpass -p $G4_DOORBELL_SSH_PASSWORD ssh ubnt@$G4_DOORBELL_HOSTNAME -f 'killall ${PROCESS_PATH}'`,
+    );
+  } catch (error) {
+    logError(`${error}`);
   }
 };
 
